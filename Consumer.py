e617569e3a1b7e29e27c8b510f11cc43afb5a020
Pedro Teixeira <otmpedrofaria@gmail.com>
2022-07-02 18:50:01 -0300

    Adding save json mode

--- Consumer.py
+++ Consumer.py
@@ -1,12 +1,16 @@
+from asyncio.log import logger
+from itertools import count
 from kafka import KafkaConsumer
 import pandas as pd
 import json
 import logging as log
 from data.utils import forgiving_json_deserializer
-
+import os
 
 class Consumer():
-    def __init__(self, bootstrap_server, topic, auto_offset_reset):
+    def __init__(self,channel, source, bootstrap_server, topic, auto_offset_reset):
+        self.source = source
+        self.channel = channel
         self.bootstrap_server = bootstrap_server
         self.topic = topic
         self.auto_offset_reset = auto_offset_reset
@@ -19,20 +23,20 @@ class Consumer():
     def consume_messages(self):
         # df = pd.DataFrame(columns=["datetime", "name", "message"])
 
+        self.create_folder()
+        counter = 0
+
         for msg in self.consumer:
             msg = json.loads(msg.value)
 
-            #colocar na parte bash
-            # with open('json_data.json', 'w') as outfile:
-            #     json.dump(msg, outfile)
-
-            #Colocar na parte stream esse frame
-
-            self.load_dataframe_to_mysql(msg)
-            # print(df.count())
+            self.load_dataframe(msg)
+            self.save_json_to_data_lake(msg, counter)
             
-    def load_dataframe_to_mysql(self, msg):
+            counter += 1
+            
+    def load_dataframe(self, msg):
             self.df = pd.concat([self.df,pd.DataFrame({
+                "channel" : [[self.channel]],
                 "id": [msg["id"]],
                 "datetime": [pd.to_datetime(msg["datetime"])],
                 "name": [msg["author"]["name"]],
@@ -45,14 +49,25 @@ class Consumer():
                 "message":  [msg["message"]],
                 "amountValue": [msg["amountValue"]],
                 "amountString": [msg["amountString"]],
-                "fromSource": ["Youtube"]
+                "fromSource": [self.source]
             })],ignore_index=True) 
 
             print(self.df.count())
 
-    def save_json_to_data_lake(msg):
-        pass
+    def save_json_to_data_lake(self, msg, counter):
+        
+            with open(f'.\\datalake\\bronze\\{self.channel}\\{counter}.json', 'a') as outfile:
+                
+                json.dump(msg, outfile, indent=6)
+                outfile.close
+            
+            log.info("JSON Saved")
 
+    def create_folder(self):
+        if os.path.exists(f"{os.getcwd()}\\datalake\\bronze\\{self.channel}") :
+            log.info("Folder already exisits")
+        else:
+            os.makedirs(f"{os.getcwd()}\\datalake\\bronze\\{self.channel}")
 
-consumer = Consumer(bootstrap_server="localhost:29092", topic="youtube", auto_offset_reset="earliest")
+consumer = Consumer("lofi-hiphop","youtube",bootstrap_server="localhost:29092", topic="youtube", auto_offset_reset="earliest")
 consumer.consume_messages()
--- StreamingChat.py
+++ StreamingChat.py
@@ -1,13 +1,29 @@
 import pytchat
 import time
 
-chat = pytchat.create(video_id="uIx8l2xlYVY")
-while chat.is_alive():
-    for c in chat.get().items:
-        print(c.json())
-    time.sleep(5)
-    '''
-    # Each chat item can also be output in JSON format.
-    for c in chat.get().items:
-        print(c.json())
-    '''  
+import os
+import json
+import pandas as pd 
+
+# chat = pytchat.create(video_id="uIx8l2xlYVY")
+# while chat.is_alive():
+#     for c in chat.get().items:
+#         print(c.json())
+#     time.sleep(5)
+#     '''
+#     # Each chat item can also be output in JSON format.
+#     for c in chat.get().items:
+#         print(c.json())
+#     '''  
+
+# print(os.getcwd("datalake"))
+
+
+# json.load(".\\datalake\\bronze\\lofi-hiphop\\data_concentrated.txt")
+
+with open('.\\datalake\\bronze\\lofi-hiphop\\data_concentrated.txt') as f:
+   data = json.load(f)
+
+print(data)
+
+# df = pd.read_json(".\\datalake\\bronze\\lofi-hiphop\\data_concentrated.txt")
